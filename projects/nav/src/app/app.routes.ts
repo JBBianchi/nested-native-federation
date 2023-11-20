ac1e061d44be76905db9ecbb898b300ff0f1008a
JBBianchi <jb.bianchi@neuroglia.io>
2023-11-20 08:46:58 +0100

    invoked initFederation each time a route is loaded

--- projects/nav/src/app/app.routes.ts
+++ projects/nav/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HelloComponent } from './hello.component';
-import { loadRemoteModule } from '@angular-architects/native-federation';
+import { initFederation, loadRemoteModule, processRemoteInfo } from '@angular-architects/native-federation';
 import { NavComponent } from './nav.component';
+
+
 
 export const routes: Routes = [
     {
@@ -11,12 +13,12 @@ export const routes: Routes = [
             {
                 path: 'dashboard',
                 loadChildren: () =>
-                    loadRemoteModule('dashboard', './Component')//.then((m) => m.AppComponent)
+                    initFederation('/assets/federation.manifest.json').then(() => loadRemoteModule('dashboard', './Component'))//.then((m) => m.AppComponent)
             },
             {
                 path: 'profile',
                 loadChildren: () =>
-                    loadRemoteModule('profile', './Component')//.then((m) => m.AppComponent)
+                    initFederation('/assets/federation.manifest.json').then(() => loadRemoteModule('profile', './Component'))//.then((m) => m.AppComponent)
             },
             {
                 path: '',
